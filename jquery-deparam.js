a74d81b7067a6498575b14cd4082e7d62e0a5bed
James Addison <jay@jp-hosting.net>
2020-05-06 17:27:51 +0100

    Standalone keys: Port implementation from cowboy/jquery-bbq#60

--- jquery-deparam.js
+++ jquery-deparam.js
@@ -19,7 +19,7 @@
         global.deparam = deparam(global.jQuery); // assume jQuery is in global namespace
     }
 })(function ($) {
-    var deparam = function( params, coerce ) {
+    var deparam = function( params, coerce, standaloneKeys ) {
         var obj = {},
         coerce_types = { 'true': !0, 'false': !1, 'null': null };
 
@@ -68,48 +68,59 @@
                     : coerce_types[val] !== undefined           ? coerce_types[val] // true, false, null
                     : val;                                                          // string
                 }
+            }
 
-                if ( keys_last ) {
-                    // Complex key, build deep object structure based on a few rules:
-                    // * The 'cur' pointer starts at the object top-level.
-                    // * [] = array push (n is set to array length), [n] = array if n is
-                    //   numeric, otherwise object.
-                    // * If at the last keys part, set the value.
-                    // * For each keys part, if the current level is undefined create an
-                    //   object or array based on the type of the next keys part.
-                    // * Move the 'cur' pointer to the next level.
-                    // * Rinse & repeat.
-                    for ( ; i <= keys_last; i++ ) {
-                        key = keys[i] === '' ? cur.length : keys[i];
-                        cur = cur[key] = i < keys_last
-                        ? cur[key] || ( keys[i+1] && isNaN( keys[i+1] ) ? {} : [] )
-                        : val;
-                    }
+            else if ( param.length === 1 && key ) {
+                // No value was defined, so set something meaningful.
+                val = coerce
+                  ? standaloneKeys
+                    ? null
+                    : undefined
+                  : '';
 
-                } else {
-                    // Simple key, even simpler rules, since only scalars and shallow
-                    // arrays are allowed.
+                if (!standaloneKeys) {
+                    // Backwards-compatibility:
+                    // * Prior to the introduction of the standaloneKeys parameter, keys
+                    //   without corresponding values would result in an undefined entry
+                    keys_last = false;
+                    obj[key] = undefined;
+                }
+            }
 
-                    if ( Object.prototype.toString.call( obj[key] ) === '[object Array]' ) {
-                        // val is already an array, so push on the next value.
-                        obj[key].push( val );
-
-                    } else if ( {}.hasOwnProperty.call(obj, key) ) {
-                        // val isn't an array, but since a second value has been specified,
-                        // convert val into an array.
-                        obj[key] = obj[key] !== undefined ? [ obj[key], val ] : val;
-
-                    } else {
-                        // val is a scalar.
-                        obj[key] = val;
-                    }
+            if ( keys_last ) {
+                // Complex key, build deep object structure based on a few rules:
+                // * The 'cur' pointer starts at the object top-level.
+                // * [] = array push (n is set to array length), [n] = array if n is
+                //   numeric, otherwise object.
+                // * If at the last keys part, set the value.
+                // * For each keys part, if the current level is undefined create an
+                //   object or array based on the type of the next keys part.
+                // * Move the 'cur' pointer to the next level.
+                // * Rinse & repeat.
+                for ( ; i <= keys_last; i++ ) {
+                    key = keys[i] === '' ? cur.length : keys[i];
+                    cur = cur[key] = i < keys_last
+                    ? cur[key] || ( keys[i+1] && isNaN( keys[i+1] ) ? {} : [] )
+                    : val;
                 }
 
             } else if ( key ) {
-                // No value was defined, so set something meaningful.
-                obj[key] = coerce
-                ? undefined
-                : '';
+                // Simple key, even simpler rules, since only scalars and shallow
+                // arrays are allowed.
+
+                if ( Object.prototype.toString.call( obj[key] ) === '[object Array]' ) {
+                    // val is already an array, so push on the next value.
+                    obj[key].push( val );
+
+                } else if ( {}.hasOwnProperty.call(obj, key) ) {
+                    // val isn't an array, but since a second value has been specified,
+                    // convert val into an array.
+                    obj[key] = obj[key] === undefined ? val : [ obj[key], val ];
+
+                } else {
+                    // val is a scalar.
+                    obj[key] = val;
+                }
             }
         });
 
